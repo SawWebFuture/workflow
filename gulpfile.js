57bf2fb6fe78bdba98b0ab4dcd6a89ae8089f1bc
Scott Williams <scottalanw@gmail.com>
2015-10-10 21:34:13 +0200

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,9 @@ var jsSources = [
 	'components/scripts/shadowbox.js'
 ];
 var sassSources = ['components/sass/style.scss'];
-	
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
+
 gulp.task('coffee', function (){
 	gulp.src(coffeeSources)
 		.pipe(coffee({bare: true})
@@ -46,6 +48,8 @@ gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function(){
@@ -55,5 +59,15 @@ gulp.task('connect', function(){
 	});
 });
 
-gulp.task('default', ['compass', 'js', 'coffee', 'watch', 'connect']);
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('default', ['json', 'html', 'compass', 'js', 'coffee', 'watch', 'connect']);
 
